3d3db859fdaed6ca771f1f13b9f45561bbd7a8bb
PereMekna <dupont.louis4@gmail.com>
2016-03-15 16:53:33 +0100

    test mail 2

--- test/testmail.php
+++ test/testmail.php
@@ -1,2 +1,22 @@
-<?php mail('jerome.dupire@itech-informatique.com', 'salut', 'salut');
-mail('dupont.louis4@gmail.com', 'salut', 'salut'); ?>
+<?php
+$destinataire = 'jerome.dupire@itech-informatique.com';
+$expediteur = 'coucou@itech-informatique.com';
+$objet = "coucou";
+$message = "Description du ticket : <br />";
+$headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+$headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n";
+$headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
+$headers .= 'From: "ITECH"<'.$expediteur.'>'."\n"; // Expediteur
+$headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
+mail($destinataire, $objet, $message, $headers);
+$destinataire = 'dupont.louis4@gmail.com';
+$expediteur = 'coucou@itech-informatique.com';
+$objet = "coucou";
+$message = "Description du ticket : <br />";
+$headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+$headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n";
+$headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
+$headers .= 'From: "ITECH"<'.$expediteur.'>'."\n"; // Expediteur
+$headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
+mail($destinataire, $objet, $message, $headers);
+?>
