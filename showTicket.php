c6b94353b3242dd44cbf21c75b77a801d11d1625
PereMekna <dupont.louis4@gmail.com>
2016-04-08 14:47:59 +0200

    zqdzq

--- showTicket.php
+++ showTicket.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <html lang="fr">
   <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
