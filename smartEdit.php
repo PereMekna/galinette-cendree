04f38cdd7ede955c025e99f8cdd665734057c567
PereMekna <dupont.louis4@gmail.com>
2016-04-08 15:54:50 +0200

    add favicon

--- smartEdit.php
+++ smartEdit.php
@@ -5,6 +5,7 @@ require_once('dbConn.php'); ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='shortcut icon' type='image/x-icon' href='./favicon.ico' />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Licenses SMART</title>
 
